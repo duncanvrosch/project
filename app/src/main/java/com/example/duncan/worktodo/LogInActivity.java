d79a58f510289709bf1225cb359c180f94c67b32
Duncan <duncandag@gmail.com>
2019-01-09 17:11:19 +0100

    wednesday

--- app/src/main/java/com/example/duncan/worktodo/LogInActivity.java
+++ app/src/main/java/com/example/duncan/worktodo/LogInActivity.java
@@ -1,13 +1,54 @@
 package com.example.duncan.worktodo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LogInActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+    }
+
+    private void validate(String userName, String userPassword){
+        if((userName == "Werkgever") && (userPassword == "Werkgever")){
+            Intent intent = new Intent(LogInActivity.this, MenuWGActivity.class);
+            startActivity(intent);
+        }
+
+        else{
+            counter--;
+
+            Info.setText("Remaining attempts: " + String.valueOf(counter));
+
+            if(counter == 0){
+                Login.setEnabled(false);
+            }
+        }
     }
 }
