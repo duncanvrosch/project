086cd7be1ea36368a4a4ef22556402fc3094fa93
Duncan <duncandag@gmail.com>
2019-01-18 15:10:55 +0100

    tryingtodelete

--- app/src/main/java/com/example/duncan/worktodo/ToDoListDetailWGActivity.java
+++ app/src/main/java/com/example/duncan/worktodo/ToDoListDetailWGActivity.java
@@ -3,9 +3,26 @@ package com.example.duncan.worktodo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ToDoListDetailWGActivity extends AppCompatActivity {
+
+    Score score;
+    int removalId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +51,68 @@ public class ToDoListDetailWGActivity extends AppCompatActivity {
         itemName.setText(name);
         itemTimestamp.setText(timestamp);
         itemPriority.setText(priority);
+    }
+
+    public void onClickDONETODOITEM(View v) {
+
+        Intent intent = getIntent();
+        final Score selectedItem = (Score) intent.getSerializableExtra("selected_item");
+
+        String url = "https://ide50-duncanvrosch.legacy.cs50.io:8080/todolist";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+
+                String title = selectedItem.getTitle();
+                for (int i = 0; i < response.length(); i++) {
+
+                    try {
+                        JSONObject test = response.getJSONObject(i);
+                        String checkId = test.getString("theId");
+
+                        if (title.equals(checkId)) {
+                            removalId = test.getInt("id");
+                            break;
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                RequestQueue deleteQueue = Volley.newRequestQueue(getApplicationContext());
+
+                String deleteUrl = String.format("https://ide50-duncanvrosch.legacy.cs50.io:8080/todolist/%d", removalId);
+
+                StringRequest dr = new StringRequest(Request.Method.DELETE, deleteUrl,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                );
+                deleteQueue.add(dr);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+        queue.add(jsonArrayRequest);
+
+        Intent intentt = new Intent(ToDoListDetailWGActivity.this, ToDoListWGActivity.class);
+        startActivity(intentt);
     }
 }
